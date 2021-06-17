a1b62f2527ef1183e16f922ffe33b9c9d40c06aa
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2021-06-17 10:20:53 +0530

    build: configure lint and workflow for lint

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+// eslint-disable-next-line new-cap
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('Hello World from TNB List User Service');
 });
 
